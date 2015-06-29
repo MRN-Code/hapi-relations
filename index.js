7d171f551ec3928d3fa8ac71a4e978f7e604d981
Ross Kelly <rkelly@mrn.org>
2015-06-29 16:09:42 -0600

    initial relations commit

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 'use strict';
-
-var config = require('config');
-
-console.log('this is the value of a required config option: `%s`', config.get('required'));
-
-if (config.has('optional')) {
-    console.log('this is the value of an optional config option: `%s`', config.get('optional'));
-} else {
-    console.log('no optional config option set');
-}
-
+module.exports = require('./lib/relations.js');
